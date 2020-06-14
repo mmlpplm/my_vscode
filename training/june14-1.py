2ad130e11c956d916a5c980b69248f101b02ff91
hanstar lee <lihanxing123456@16.com>
2020-06-14 08:49:57 +0800

    june14-1

--- training/june14-1.py
+++ training/june14-1.py
@@ -10,3 +10,30 @@ y = temp
 
 print('交换后 x 的值为：{}'.format(x))
 print('交换后 y 的值为：{}'.format(y))
+
+#写法2
+x = input('请输入 x 值：')
+y = input('请输入 y 值：')
+#不使用临时变量
+x,y = y,x
+print('交换后 x 的值为：{}'.format(x))
+print('交换后 y 的值为：{}'.format(y))
+
+#写法3
+x = float(input('请输入 x 值：'))
+y = float(input('请输入 y 值：'))
+#不使用临时变量
+x = x + y
+y = x - y
+x = x - y
+print('交换后 x 的值为：{}'.format(x))
+print('交换后 y 的值为：{}'.format(y))
+
+#写法4
+x = int(input('请输入 x 值：'))
+y = int(input('请输入 y 值：'))
+x = x ^ y
+y = x ^ y
+x = x ^ y
+print('交换后 x 的值为：{}'.format(x))
+print('交换后 y 的值为：{}'.format(y))
