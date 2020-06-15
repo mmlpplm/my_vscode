a42c52c6cadfe28e1989291e9785191efb961560
hanstar lee <lihanxing123456@16.com>
2020-06-15 20:50:39 +0800

    june15-1

---
+++ training/june15-1.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF -8 -*-
+# 十进制转二进制、八进制、十六进制
+dec = int(input('输入数字：'))
+
+print('十进制数为：',dec)
+print('转换为二进制数为：',bin(dec))
+print('转换为八进制数为：',oct(dec))
+print('转换为十六进制数为：',hex(dec))
+
